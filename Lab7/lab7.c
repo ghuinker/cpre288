c19e6a6d887b4f2f5e309f8be6c223fae7b46f85
ghuinker <ghuinker@iastate.edu>
2021-03-24 13:38:05 -0500

    Lab 7

--- Lab7/lab7.c
+++ Lab7/lab7.c
@@ -11,38 +11,57 @@
 #include "lcd.h"
 #include "adc.h"
 #include "button.h"
+#include "cyBot_uart.h"
 #include <math.h>
+
+void sendLine(char *str)
+{
+    int i;
+    int len = strlen(str);
+
+    for (i = 0; i < len; i++)
+    {
+        cyBot_sendByte(str[i]);
+    }
+    cyBot_sendByte('\r');
+    cyBot_sendByte('\n');
+}
 
 int main(void) {
     button_init();
 	timer_init(); // Must be called before lcd_init(), which uses timer functions
 	lcd_init();
 	adc_init();
-
-	lcd_printf("Hello World");
+	cyBot_uart_init();
 
 	int value;
 	int i;
-	int total;
+
 	int distance;
+	char output[20];
+
 
 
     while(1){
         value = adc_read();
-        distance = 276 - (34.1 * log(value));
-        lcd_printf("Raw: %d Dist: %d", value, distance);
+        distance = 111075 * pow(value, -1.16);
+        sprintf(output, "%d, %d", value, distance);
+        sendLine(output);
 
-        for(i =0; i< 1000000; i++){};
+        while(!button_getButton()){};
+        for(i =0; i < 1000000; i++){};
     }
 
+//  int COUNTS = 100000;
+//  int total;
 //	while(1){
-//	    for(i = 0; i < 1000; i++){
+//	    for(i = 0; i < COUNTS; i++){
 //	        total += adc_read();
 //	    }
-//	    lcd_printf("Average: %d", total/1000);
+//	    lcd_printf("Average: %d", total/COUNTS);
 //
 //	    while(!button_getButton()){};
-//	    for(i =0; i < 1000000; i++){};
+//	    for(i =0; i < 10000; i++){};
 //	    total = 0;
 //	}
 
